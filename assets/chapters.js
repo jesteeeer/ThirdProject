c19f640736002d97032ae34005d608dceafb49df
Jester <johnjesterluciriaga4@gmail.com>
2025-11-23 13:29:18 +0800

    add assets for css, images, fonts and js

---
+++ assets/chapters.js
@@ -0,0 +1,32 @@
+var chapters =  [
+    {
+        "image": "assets/images/chap1.png",
+        "chapter": "Chapter 1",
+        "title": "Colter"
+    },
+    {
+        "image": "assets/images/chap2.jpg",
+        "chapter": "Chapter 2",
+        "title": "Horshoe Overlook"
+    },
+    {
+        "image": "assets/images/chap3.png",
+        "chapter": "Chapter 3",
+        "title": "Clemens Point"
+    },
+    {
+        "image": "assets/images/chap 4.png",
+        "chapter": "Chapter 4",
+        "title": "Saint Denis"
+    },
+    {
+        "image": "assets/images/chap5.png",
+        "chapter": "Chapter 5",
+        "title": "Guarma"
+    },
+    {
+        "image": "assets/images/chap6.png",
+        "chapter": "Chapter 6",
+        "title": "Beaver Hollow"
+    }
+]
---
+++ assets/characters.js
@@ -0,0 +1,62 @@
+var characters = [
+    {
+        "name": "Arthur Morgan",
+        "image": "assets/images/arthur.jpg",
+        "description": "A walking outlaw making the line between redemption and ruin"
+    },
+    {
+        "name": "Dutch Van Der Linde",
+        "image": "assets/images/dutchh.jpg",
+        "description": "A leader who believes the world can be rewritten—so long as the gang keeps faith in his grand ideas."
+    },
+    {
+        "name": "John Marston",
+        "image": "assets/images/john.jpg",
+        "description": "A reformed gunslinger fighting for a life he was never meant to have."
+    },
+    {
+        "name": "Micah Bell",
+        "image": "assets/images/Micah.jpg",
+        "description": "A sharp-tongued drifter with a taste for danger and opportunity."
+    },
+    {
+        "name": "Hosea Matthews",
+        "image": "assets/images/Hosea_Matthews.jpg",
+        "description": "A wise old conman whose wit is sharper than any blade."
+    },
+    {
+        "name": "Sadie Adler",
+        "image": "assets/images/sadie.png",
+        "description": "A fiery widow rebord as one of the West's deadliest guns."
+    },
+    {
+        "name": "Charles Smith",
+        "image": "assets/images/charles.jpg",
+        "description": "A quiet warrior guided by honor in a dishonorable world."
+    },
+    {
+        "name": "Bill Williamson",
+        "image": "assets/images/Bill.jpg",
+        "description": "A rough-edged bruiser forever wrestling with his own pride."
+    },
+    {
+        "name": "Javier Escuella",
+        "image": "assets/images/javier.jpg",
+        "description": "A loyal outlaw driven by passion, pride, and old songs."
+    },
+    {
+        "name": "Lenny Summers",
+        "image": "assets/images/lenny.jpg",
+        "description": "A sharp young rider chasing freedom faster than bullets."
+    },
+    {
+        "name": "Abigail Roberts",
+        "image": "assets/images/abigail.jpg",
+        "description": "A hardened survivor who'll do anything to protect her family."
+    },
+    {
+        "name": "Leopold Strauss",
+        "image": "assets/images/strauss.jpg",
+        "description": "A cold banker whose polite smile hides an unforgiving grip."
+    }
+]
